61a0137f09f2d77e0e57953a64b5b792c7169dc9
Juan Pablo Amador <ajuan.ad@gmail.com>
2019-08-12 21:39:23 +0200

    Definidas las variables-

---
+++ P6a/script.js
@@ -0,0 +1,5 @@
+var nombreYapellido = 'Juan Pablo Amador Díaz'
+var miEdad = 27
+var diasSemana = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
+var gravedad = 9.80665
+var tresPrimerosNumerosImpares = [1,3,5]
