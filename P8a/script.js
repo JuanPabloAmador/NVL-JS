a198854aeecd24573b97456c388dbc787f99f6ab
Juan Pablo Amador <ajuan.ad@gmail.com>
2019-08-15 14:30:08 +0200

    P8a manejo de cadenas, remplazando Amigo por amigo, con .toLowerCase, .replace, .substring

---
+++ P8a/script.js
@@ -0,0 +1,8 @@
+var cadena = "Amigo";
+var cadenaV1 = cadena.toLowerCase();
+var cadenaV2 = cadena.replace("A","a");
+var cadenaV3 = "a" + cadena.substring(1)
+
+console.log(cadenaV1);
+console.log(cadenaV2);
+console.log(cadenaV3);
